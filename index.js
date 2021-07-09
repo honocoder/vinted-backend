160b34a1ea9824678275904aaef3f682f87f9329
Jimy Marletta <jimymarletta@mbp-de-jimy.home>
2021-07-09 15:31:42 +0200

    initial

---
+++ index.js
@@ -0,0 +1,44 @@
+// Import d'ExpressJS, Formidable et Mongoose
+const express = require("express");
+const formidable = require("express-formidable");
+const mongoose = require("mongoose");
+const cloudinary = require("cloudinary").v2;
+const isAuthenticated = require("./middlewares/isAuthenticated");
+const cors = require("cors");
+
+require("dotenv").config();
+// Initialisation du serveur
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+// Initialisation de Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+// Connexion à la db
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// Import des routes
+const userRoutes = require("./routes/users");
+const offerRoutes = require("./routes/offers");
+app.use(userRoutes);
+app.use(offerRoutes);
+
+// Gestion des routes inexistantes
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Page not found" });
+});
+
+// Écoute d'un port (3000)
+app.listen(process.env.PORT, () => {
+  console.log("Server started");
+});
---
+++ routes/offers.js
@@ -0,0 +1,114 @@
+// Appel d'Express et initialisation des routers
+const express = require("express");
+const formidable = require("express-formidable");
+const cloudinary = require("cloudinary").v2;
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const router = express.Router();
+
+// Import des Models
+const Offer = require("../models/Offer");
+const User = require("../models/User");
+
+// Route Publish
+router.post("/offer/publish", isAuthenticated, async (req, res) => {
+  try {
+    // Création d'une nouvelle Offer
+    const newOffer = new Offer({
+      product_name: req.fields.title,
+      product_description: req.fields.description,
+      product_price: req.fields.price,
+      product_details: [
+        { MARQUE: req.fields.brand },
+        { TAILLE: req.fields.size },
+        { ÉTAT: req.fields.condition },
+        { COULEUR: req.fields.color },
+        { EMPLACEMENT: req.fields.city },
+      ],
+      owner: req.user,
+    });
+
+    // Upload de l'image sur Cloudinary
+    const result = await cloudinary.uploader.upload(req.files.picture.path, {
+      folder: `vinted/offers/${newOffer.id}`,
+    });
+
+    // Ajouter le résultat de l'upload dans newOffer
+    newOffer.product_image = result;
+
+    // Sauvegarde de l'Offer dans la db
+    await newOffer.save();
+
+    // Réponse au client
+    res.status(200).json(newOffer);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.get("/offers", async (req, res) => {
+  try {
+    // Créer un objet vide pour les filtres
+    const filters = {};
+    let sort = {};
+    // Remplir l'objet avec des paires clés/valeurs (grâce à des conditions)
+    if (req.query.title) {
+      filters.product_name = new RegExp(req.query.title, "i"); // ajoute une clé product_name à l'objet filters qui a pour valeur req.query.title(+RegExp)
+    }
+
+    if (req.query.priceMin) {
+      filters.product_price = { $gte: Number(req.query.priceMin) };
+    }
+
+    if (req.query.priceMax) {
+      if (filters.product_price) {
+        filters.product_price.$lte = Number(req.query.priceMax);
+      } else {
+        filters.product_price = { $lte: Number(req.query.priceMax) };
+      }
+    }
+
+    if (req.query.sort === "price-asc") {
+      sort = { product_price: 1 };
+    } else if (req.query.sort === "price-desc") {
+      sort = { product_price: -1 };
+    }
+
+    // let page;
+    const limit = Number(req.query.limit);
+
+    if (Number(req.query.page < 1)) {
+      page = 1;
+    } else {
+      page = Number(req.query.page);
+    }
+
+    const offers = await Offer.find(filters)
+      .populate({ path: "owner", select: "account" })
+      .sort(sort)
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    const count = await Offer.countDocuments(filters);
+
+    res.status(200).json({
+      count: count,
+      offers: offers,
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.get("/offer/:id", async (req, res) => {
+  try {
+    const offer = await Offer.findById(req.params.id).populate({
+      path: "owner",
+      select: "account",
+    });
+    res.status(200).json(offer);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
