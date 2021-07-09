50f6fb56dff61678e54a9bc4c725a3b0d921366c
Jimy Marletta <jimymarletta@mbp-de-jimy.home>
2021-07-09 16:24:12 +0200

    fixed bug first user

--- routes/users.js
+++ routes/users.js
@@ -15,47 +15,47 @@ const Offer = require("../models/Offer");
 router.post("/user/signup", async (req, res) => {
   try {
     // Vérifier que l'email reçu n'exite pas déjà dans la db
-    const user = await User.findOne({ email: req.fields.email });
-    if (!user) {
-      // Password
-      // Générer SALT
-      const salt = uid2(16);
-      // console.log("salt ==>", salt);
+    // const user = await User.findOne({ email: req.fields.email });
+    // if (!user) {
+    // Password
+    // Générer SALT
+    const salt = uid2(16);
+    // console.log("salt ==>", salt);
 
-      // Générer un HASH
-      const hash = SHA256(req.fields.password + salt).toString(encBase64);
-      // console.log("hash ==>", hash);
+    // Générer un HASH
+    const hash = SHA256(req.fields.password + salt).toString(encBase64);
+    // console.log("hash ==>", hash);
 
-      // Générer un TOKEN
-      const token = uid2(64);
-      // console.log("token ==>", token);
+    // Générer un TOKEN
+    const token = uid2(64);
+    // console.log("token ==>", token);
 
-      // Créer un nouveau User
-      const newUser = new User({
-        email: req.fields.email,
-        account: {
-          username: req.fields.username,
-          phone: req.fields.phone,
-        },
-        token: token,
-        hash: hash,
-        salt: salt,
-      });
-      // Sauvegarde du nouveau User dans la db
-      await newUser.save();
+    // Créer un nouveau User
+    const newUser = new User({
+      email: req.fields.email,
+      account: {
+        username: req.fields.username,
+        phone: req.fields.phone,
+      },
+      token: token,
+      hash: hash,
+      salt: salt,
+    });
+    // Sauvegarde du nouveau User dans la db
+    await newUser.save();
 
-      // Réponse au client
-      res.status(200).json({
-        _id: newUser._id,
-        token: token,
-        account: {
-          username: req.fields.username,
-          phone: req.fields.phone,
-        },
-      });
-    } else {
-      res.status(409).json({ message: "This email already has an account" });
-    }
+    // Réponse au client
+    res.status(200).json({
+      _id: newUser._id,
+      token: token,
+      account: {
+        username: req.fields.username,
+        phone: req.fields.phone,
+      },
+    });
+    // } else {
+    //   res.status(409).json({ message: "This email already has an account" });
+    // }
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
